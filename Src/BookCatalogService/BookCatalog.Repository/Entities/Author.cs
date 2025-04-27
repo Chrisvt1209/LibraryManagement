0021573e84a23c8b14c4501d1a0f37ecba5eabd6
Chris van Tiggelen <515073@student.fontys.nl>
2025-04-27 14:46:00 +0200

    Added entities and editorconfig for BookCatalogService

---
+++ Src/BookCatalogService/BookCatalog.Repository/Entities/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace BookCatalog.Repository.Entities;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? Biography { get; set; }
+}
---
+++ Src/BookCatalogService/BookCatalog.Repository/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace BookCatalog.Repository.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public byte[]? CoverImage { get; set; }
+    public DateOnly PublicationDate { get; set; }
+    public decimal Price { get; set; }
+    public Genre Genre { get; set; }
+    public Author Author { get; set; }
+    public int AuthorId { get; set; }
+}
